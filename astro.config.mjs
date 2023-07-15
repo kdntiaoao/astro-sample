c5d97b18327c966b3ab4ff873c14ca40fa55becd
iadokonat <iadokonat0106@gmail.com>
2023-07-15 12:42:29 +0900

    add: Image Sample Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import vue from '@astrojs/vue'
-
 import tailwind from '@astrojs/tailwind'
+
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), tailwind()],
+  integrations: [
+    react(),
+    vue(),
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
   outDir: './dist/test',
   base: '/test',
 })
